eaea3fc01a2a14b46679321c5d5a853a238ac655
evalsta@gmail.com <Panorama2030>
2018-01-03 15:34:32 +0200

    redaguota

---
+++ js/script2.js
@@ -0,0 +1,14 @@
+$ (document).ready(function() {
+
+    $('input[id=table]').onclick('save', function () {
+        var AutomobilioNr = document.getElementById(AutomobilioNr).value;
+});
+
+    $('input[id=NuvaziuotasKelias]').onclick('save', function () {
+        var NuvaziuotasKelias = document.getElementById(NuvaziuotasKelias).value;
+    });
+
+    $('input[id=Laikas]').onclick('save', function () {
+        var Laikas = document.getElementById(Laikas).value;
+    });
+});
